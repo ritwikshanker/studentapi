143f2ac5b03ff4154063ec42834c170e50193f61
Ritwik <ritwikshanker@gmail.com>
2019-03-20 17:26:31 +0530

    All Basic Endpoints Implemented

---
+++ models/students.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var studentSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    stuID: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    batch: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: String,
+        required: true
+    },
+}, {
+    timestamps: true
+});
+var Students = mongoose.model('Student', studentSchema);
+
+module.exports = Students;
